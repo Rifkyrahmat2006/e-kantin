896d30ad053149ba0621f9f1d05f038ddd3f30a3
Rifkyrahmat2006 <rifkyrahmat2006@gmail.com>
2025-12-12 10:54:53 +0700

    fix: changing button Google OAuth to pill.

--- resources/js/components/login-form.tsx
+++ resources/js/components/login-form.tsx
@@ -108,12 +108,12 @@ export function LoginForm({
               useOneTap
               theme="outline"
               size="large"
-              shape="rectangular"
+              shape="pill"
               width="100%"
             />
           </div>
           <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground bg-white">
+            <span className="relative z-10 bg-background px-2 text-muted-foreground">
               Or continue with
             </span>
           </div>
